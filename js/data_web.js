c2d617efd2c6e1f04dab764dde0c1d4497e348f1
kgarcia18 <kgarcia@scl.edu.gt>
2024-03-13 21:33:43 +0000

    diseño de pagina

---
+++ js/data_web.js
@@ -0,0 +1,26 @@
+let lista_webs = [
+    {
+        titulo: "Titulo",
+        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
+        link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
+        githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+    },
+    {
+        titulo: "Titulo",
+        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
+        link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
+        githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+    },
+    {
+        titulo: "Titulo",
+        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
+        link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
+        githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+    }
+    
+];
+
+export{lista_webs};
--- js/main.js
+++ js/main.js
@@ -4,10 +4,8 @@ let div_root = document.querySelector(".root");
 let componenetes = `
     <div class="header">${header}</div>
     <div class="perfil">${perfil}</div>
-    <div class="pagina">
-        <div class="item1"></div>
-        <div class="item2"></div>
-    </div>
+    <div class="paginas"></div>
+    <div class="soporte"></div>
 `;
 
 div_root.innerHTML = componenetes;
