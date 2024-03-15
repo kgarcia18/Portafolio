de6243b07e4e7dcd8a5e33e8709f109b31c8712e
kgarcia18 <kgarcia@scl.edu.gt>
2024-03-15 10:22:00 +0000

    diseño del item

--- js/data_web.js
+++ js/data_web.js
@@ -1,24 +1,24 @@
 let lista_webs = [
     {
-        titulo: "Titulo",
-        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
+        titulo: "Memoria",
+        img: "https://github.com/kgarcia18/img/blob/main/img/Memoria.png?raw=true",
         link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
         githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+        descripcion: "Este juego clásico y entretenido está diseñado para desafiar y estimular las mentes jóvenes mientras se divierten. En este juego, los niños se enfrentan a una serie de cartas colocadas boca abajo, cada una con una imagen diferente."
     },
     {
-        titulo: "Titulo",
-        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
-        link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
-        githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+        titulo: "Página",
+        img: "https://github.com/kgarcia18/img/blob/main/img/Pagina.png?raw=true",
+        link_repo: "https://github.com/kgarcia18/1P_Prueba1?tab=readme-ov-file",
+        githun_page: "https://kgarcia18.github.io/1P_Prueba1/",
+        descripcion: "Una página web básica creada en HTML es una interfaz simple diseñada para mostrar contenido estático en línea. Utilizando HTML (HyperText Markup Language), esta página puede contener texto, imágenes y enlaces organizados en un diseño estructurado."
     },
     {
-        titulo: "Titulo",
-        img: "https://github.blog/wp-content/uploads/2023/01/1200x640-2.png?resize=1200%2C640",
-        link_repo: "https://github.com/kgarcia18/kgarcia18-P1_JuegoDeMemoria_2024_VBach",
-        githun_page: "https://kgarcia18.github.io/kgarcia18-P1_JuegoDeMemoria_2024_VBach/",
-        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt deserunt vel dolor aspernatur, laudantium possimus quidem laboriosam expedita! Tenetur quis explicabo itaque odio ex! Obcaecati voluptatibus voluptate earum cupiditate."
+        titulo: "Trivago",
+        img: "https://github.com/kgarcia18/img/blob/main/img/Trivago.png?raw=true",
+        link_repo: "https://github.com/kgarcia18/2P_Trivago?tab=readme-ov-file",
+        githun_page: "https://kgarcia18.github.io/2P_Trivago/",
+        descripcion: "Una página básica de diseño de viajes es una plataforma digital que simplifica la planificación de viajes. Permite a los usuarios explorar destinos, buscar alojamientos, encontrar actividades y reservar vuelos o transporte de manera eficiente."
     }
     
 ];
