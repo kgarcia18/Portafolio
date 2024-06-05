394b2a4f90ab153183534bee69d96393e890f3fa
kgarcia <kgarcia@scl.edu.gt>
2024-06-05 12:14:22 -0600

    Versiones del Portafolio

--- js/stacks.js
+++ js/stacks.js
@@ -7,10 +7,10 @@ let stacks = `
        <div class="items"><img src="https://raw.githubusercontent.com/ROCKEMMA/img/d538b23783cd8f93bb587a9766bf71023257f1d7/stacks/vscode.svg" alt=""></div>
        <div class="items"><img src="https://raw.githubusercontent.com/ROCKEMMA/img/d538b23783cd8f93bb587a9766bf71023257f1d7/stacks/figma.svg" alt=""></div>
        <div class="items"><img src="https://raw.githubusercontent.com/ROCKEMMA/img/d538b23783cd8f93bb587a9766bf71023257f1d7/stacks/notion.svg" alt=""></div>
-       <div>html</div>
-       <div>css</div>
-       <div>javascript</div>
-       <div>java</div>
+       <div class="items1">html</div>
+       <div class="items2">css</div>
+       <div class="items3">javascript</div>
+       <div class="items4">java</div>
        <div>GitHub</div>
        <div>vscode</div>
        <div>figma</div>
